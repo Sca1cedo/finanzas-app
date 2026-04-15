7b54b925eaa4bd7fa79b6afb587dc4346f6c9fbb
Sca1cedo <sebastianliveforever@gmail.com>
2026-04-15 12:59:12 +0000

    register2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,3 @@
-import { useEffect, useState } from 'react'
-import { supabase } from './lib/supabase'
-
-function App() {
-  const [transactions, setTransactions] = useState([])
-
-  useEffect(() => {
-    getTransactions()
-  }, [])
-
-  async function getTransactions() {
-    const { data } = await supabase
-      .from('transactions')
-      .select('*')
-
-    setTransactions(data)
-  }
-
-  return (
-    <div>
-      <h1>Transacciones</h1>
-
-      {transactions.map((t) => (
-        <div key={t.id}>
-          {t.tipo} - {t.monto}
-        </div>
-      ))}
-    </div>
-  )
-}
-
-export default App
-
 import { useState } from 'react'
 import { signUp, signIn } from './auth'
 
